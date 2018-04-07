d6151d82dd7f54524e9454dff3c237b310e9a913
Eirik S. Morland <eirik@morland.no>
2018-04-07 15:37:08 +0200

    Add a session control service

---
+++ modules/session_control/src/SessionControlManager.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\session_control;
+
+use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+
+/**
+ * SessionControlManager service.
+ */
+class SessionControlManager {
+
+  /**
+   * Config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactory
+   */
+  protected $config;
+
+  /**
+   * Constructs a SessionControlManager object.
+   */
+  public function __construct(ConfigFactory $config) {
+    $this->config = $config;
+  }
+
+  /**
+   * Are sessions enabled?
+   */
+  public function areSessionsEnabled($year) {
+    if (date('Y') != $year) {
+      return FALSE;
+    }
+    $config = $this->config->get('session_control.settings');
+    if (!$config->get('sessions_enabled')) {
+      return FALSE;
+    }
+    if ($config->get('session_deadline') < time()) {
+      return FALSE;
+    }
+    return TRUE;
+  }
+
+}
